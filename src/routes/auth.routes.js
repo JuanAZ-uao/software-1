4acddae37a0001339fc8e61a7966f60ec0456b9b
Jeronimo <jeronimo23colonia@gmail.com>
2025-10-06 08:41:30 -0500

    modified:   src/controllers/auth.controller.js 	modified:   src/routes/auth.routes.js

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import { asyncHandler } from '../core/middlewares/async-handler.js';
-import { loginUser, registerUser, getCurrentUser } from '../controllers/auth.controller.js';
-import { validateLogin, validateRegister } from '../repositories/auth.validation.js';
+import { loginUser, registerUser, getCurrentUser, logoutUser } from '../controllers/auth.controller.js';
 
 const router = Router();
 
 router.post('/login', validateLogin, asyncHandler(loginUser));
 router.post('/register', validateRegister, asyncHandler(registerUser));
 router.get('/me', asyncHandler(getCurrentUser));
+router.post('/logout', asyncHandler(logoutUser));
 
 export const authRouter = router;
 import { forgotPassword, resetPassword } from '../controllers/auth.controller.js';
